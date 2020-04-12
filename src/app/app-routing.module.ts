d96aba6ac3828234c366b114e604fe1d94c1a5c6
clever.ch <clever.chuky@gmail.com>
2020-04-12 17:58:29 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PublicationCreateComponent } from 'src/app/components/publication-create/publication-create.component';
+import { PublicationListComponent } from 'src/app/components/publication-list/publication-list.component';
+
+
+
+const routes: Routes = [
+  { path: 'publicationCreate', component: PublicationCreateComponent },
+  { path: 'publicationList', component: PublicationListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
