8876b1399c512461cd57c6d0d6f50f7f68f7695f
clever.ch <clever.chuky@gmail.com>
2020-04-22 18:55:25 -0300

    Update publication-list.component.ts

--- src/app/components/publication-list/publication-list.component.ts
+++ src/app/components/publication-list/publication-list.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { Observable } from "rxjs";
 import { PublicationService } from "src/app/services/publication-service/publication.service";
 import { PublicationDTO } from "src/app/model/publication-dto";
+import { DialogPublicationComponent } from '../dialog-publication/dialog-publication.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
 
 @Component({
   selector: 'app-publication-list',
@@ -13,7 +16,7 @@ export class PublicationListComponent implements OnInit {
 
   publications: Observable<PublicationDTO[]>;
 
-  constructor(private publicationService: PublicationService, private router: Router) {}
+  constructor(private publicationService: PublicationService, private router: Router, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.reloadData();
@@ -21,6 +24,19 @@ export class PublicationListComponent implements OnInit {
 
   reloadData() {
     this.publications = this.publicationService.getPublicationList();
+  }
+
+  animal: string;
+
+  openDialog(publicationDTO: PublicationDTO): void {
+    const dialogRef = this.dialog.open(DialogPublicationComponent, {
+      width: '700',
+      data: publicationDTO
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.animal = result;
+    });
   }
 
 }
