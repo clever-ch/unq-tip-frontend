6ec660e8f84be87e122c85d289acd7b5f8ad32ee
clever.ch <clever.chuky@gmail.com>
2020-04-26 18:58:51 -0300

    Create userDTO.ts

---
+++ src/app/model/userDTO.ts
@@ -0,0 +1,9 @@
+export class UserDTO {
+    id: number;
+    userName: string;
+    email: string;
+    accountName: string;
+    accountSurName: string;
+    accountTelephone: string;
+    accountAddress: string;
+}
