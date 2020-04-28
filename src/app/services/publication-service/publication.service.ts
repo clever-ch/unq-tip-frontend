7a6f2201808e0831e3618963f0ea92df62fac8a8
clever.ch <clever.chuky@gmail.com>
2020-04-28 18:50:17 -0300

    Update publication.service.ts

--- src/app/services/publication-service/publication.service.ts
+++ src/app/services/publication-service/publication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UrlManagerPublicationService } from 'src/app/utilities/urlManagerPublication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,13 @@ export class PublicationService {
 
   private baseUrl = 'http://localhost:8080/api/v1/publications';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private urlManagerPublication: UrlManagerPublicationService) { }
 
   getPublicationList(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
+  }
+
+  getAllPublicationsFound(): Observable<any> {
+    return this.http.get(`${this.urlManagerPublication.getURLAllPublicationsFound()}`);
   }
 }
