e21387c50b2a4577d1761516aba0f3936d7818d7
clever.ch <clever.chuky@gmail.com>
2020-04-28 18:49:47 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PublicationCreateComponent } from 'src/app/components/publication-create/publication-create.component';
 import { PublicationListComponent } from 'src/app/components/publication-list/publication-list.component';
-
+import { PublicationsFoundComponent } from 'src/app/components/publications-found/publications-found.component';
 
 
 const routes: Routes = [
   { path: 'publicationCreate', component: PublicationCreateComponent },
   { path: 'publications', component: PublicationListComponent },
+  { path: 'allPublicationsFound', component: PublicationsFoundComponent },
 ];
 
 @NgModule({
