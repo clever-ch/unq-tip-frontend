49b0602b4ca440cef295587f91d2c2f24fb2d102
clever.ch <clever.chuky@gmail.com>
2020-05-10 17:43:41 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from "../../src/app/services/auth-service/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'unq-tip-frontend';
+
+  constructor(public authService: AuthService) {
+  }
 }
