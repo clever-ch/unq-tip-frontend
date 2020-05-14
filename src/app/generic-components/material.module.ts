beb8d82520b6b8ca90129a6998fcf823c41d7242
clever.ch <clever.chuky@gmail.com>
2020-05-14 18:25:09 -0300

    Update material.module.ts

--- src/app/generic-components/material.module.ts
+++ src/app/generic-components/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { MatDialogModule, MatFormFieldModule, MatButtonModule, MatInputModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
-  exports: [FormsModule, MatDialogModule, MatFormFieldModule, MatButtonModule, MatInputModule]
+  exports: [FormsModule, MatDialogModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  declarations: [ErrorPageComponent]
 })
 export class MaterialModule {}
