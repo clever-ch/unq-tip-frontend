bdd2df6ecc7bd6581eecd83e3612a7c3aeee6996
clever.ch <clever.chuky@gmail.com>
2020-05-19 20:04:17 -0300

    Update userDTO.ts

--- src/app/model/userDTO.ts
+++ src/app/model/userDTO.ts
@@ -2,9 +2,9 @@ import { PersonDTO } from './PersonDTO';
 
 export class UserDTO {
     id: number;
-    userName: string;
-    email: string;
-    password: string;
-    userGuid: string;
-    personDTO: PersonDTO;
+    UserName: string;
+    Email: string;
+    Password: string;
+    UserGuid: string;
+    PersonDTO: PersonDTO;
 }
