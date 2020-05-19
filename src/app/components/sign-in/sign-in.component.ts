0921e453c23e875a5a32665d608c1be780c87f0b
nicolaspintos <nico.pintos05@gmail.com>
2020-05-19 20:47:14 -0300

    Componente registro usuario - translate - validaciones en formulario

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  
+  signin: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private appComponent: AppComponent) { }
 
   ngOnInit() {
+    this.appComponent
+    this.signin = this.formBuilder.group({
+      nomb: ['', Validators.required],
+      ape: ['', Validators.required],
+      phone: ['', Validators.required],
+      dir: ['', Validators.required],
+      loc: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(6)]]
+  });
   }
 
+  get f() { return this.signin.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.signin.invalid) {
+        return;
+    }
+
+}
+  
 }
