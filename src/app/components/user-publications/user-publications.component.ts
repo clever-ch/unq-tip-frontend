102fc5d90d798c7a4e32eed81e61ae681426dc2f
nicolaspintos <nico.pintos05@gmail.com>
2020-06-25 20:51:30 -0300

    fix publications user - translate all view

--- src/app/components/user-publications/user-publications.component.ts
+++ src/app/components/user-publications/user-publications.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
 import { PublicationService } from "src/app/services/publication-service/publication.service";
 import { PublicationDTO } from "src/app/model/publication-dto";
-import { DialogPublicationComponent } from '../dialog-publication/dialog-publication.component';
-import { MatDialog } from '@angular/material';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { LoginDTO } from 'src/app/model/loginDTO';
 import { UserDTO } from 'src/app/model/userDTO';
+
 
 
 @Component({
@@ -21,22 +20,21 @@ export class UserPublicationsComponent implements OnInit {
   publicationsLost: Observable<PublicationDTO[]>;
   publicationsFound: Observable<PublicationDTO[]>;
   images = [];
-  searchText;
+  isEmptyListLost = false;
+  isEmptyListFound = false;
 
-  constructor(private publicationService: PublicationService, public dialog: MatDialog,
-    private authService: AuthService) { }
+  constructor(private publicationService: PublicationService, private authService: AuthService) { }
 
   ngOnInit() {
     this.loginDTO = JSON.parse(localStorage.getItem("loginDTO"));
     this.GetUserLoggedInByGuid(this.loginDTO.UserGuid);
-
   }
 
   private GetUserLoggedInByGuid(userGuid: string) {
     this.authService.getUserByGuid(userGuid).subscribe(data => {
       this.userDTO = data;
-      this.publicationsLost = this.publicationService.getAllPublicationsLostByIdUser(this.userDTO.Id);
-      this.publicationsFound = this.publicationService.getAllPublicationsFoundByIdUser(this.userDTO.Id);
-    })
+      this.publicationService.getAllPublicationsLostByIdUser(this.userDTO.Id).subscribe(data => this.publicationsLost = data, error => this.isEmptyListLost = true);
+      this.publicationService.getAllPublicationsFoundByIdUser(this.userDTO.Id).subscribe(data => this.publicationsFound = data, error => this.isEmptyListFound = true);
+      });
   }
 }
