254ce90e22fcd219a3069ef69c8facd9871845d9
clever.ch <clever.chuky@gmail.com>
2020-06-28 19:18:32 -0300

    Delete publication-dto.spec.ts

--- src/app/model/publication-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PublicationDTO } from './publication-dto';
-
-describe('PublicationDTO', () => {
-  it('should create an instance', () => {
-    expect(new PublicationDTO()).toBeTruthy();
-  });
-});
